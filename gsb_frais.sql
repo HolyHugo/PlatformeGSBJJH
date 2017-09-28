b9f001e3b63619ff40cc24a2d881338541891398
Verdunj <joris42350@yahoo.fr>
2017-09-28 11:20:59 +0200

    Add files via upload
    ajout table Absence

--- gsb_frais.sql
+++ gsb_frais.sql
@@ -1,27 +1,140 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb4
+-- https://www.phpmyadmin.net/
 --
--- Contenu de la table `FraisForfait`
+-- Client :  localhost:3306
+-- Généré le :  Jeu 28 Septembre 2017 à 11:20
+-- Version du serveur :  10.1.26-MariaDB-0+deb9u1
+-- Version de PHP :  7.0.19-1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `gsb_frais`
 --
 
-INSERT INTO `FraisForfait` (`id`, `libelle`, `montant`) VALUES
-('ETP', 'Forfait Etape', 110.00),
-('KM', 'Frais Kilométrique', 0.62),
-('NUI', 'Nuitée Hôtel', 80.00),
-('REP', 'Repas Restaurant', 25.00);
+-- --------------------------------------------------------
+
 --
--- Contenu de la table `LigneFraisForfait`
+-- Structure de la table `Absence`
 --
+
+CREATE TABLE `Absence` (
+  `DateDebut` date NOT NULL,
+  `refVisiteur` varchar(4) NOT NULL,
+  `nbjour` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Etat`
+--
+
+CREATE TABLE `Etat` (
+  `id` char(2) NOT NULL,
+  `libelle` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Contenu de la table `Etat`
 --
 
 INSERT INTO `Etat` (`id`, `libelle`) VALUES
-('RB', 'Remboursée'),
 ('CL', 'Saisie clôturée'),
 ('CR', 'Fiche créée, saisie en cours'),
+('RB', 'Remboursée'),
 ('VA', 'Validée et mise en paiement');
 
 -- --------------------------------------------------------
+
+--
+-- Structure de la table `FicheFrais`
+--
+
+CREATE TABLE `FicheFrais` (
+  `idVisiteur` char(4) NOT NULL,
+  `mois` char(6) NOT NULL,
+  `nbJustificatifs` int(11) DEFAULT NULL,
+  `montantValide` decimal(10,2) DEFAULT NULL,
+  `dateModif` date DEFAULT NULL,
+  `idEtat` char(2) DEFAULT 'CR'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `FraisForfait`
+--
+
+CREATE TABLE `FraisForfait` (
+  `id` char(3) NOT NULL,
+  `libelle` char(20) DEFAULT NULL,
+  `montant` decimal(5,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Contenu de la table `FraisForfait`
+--
+
+INSERT INTO `FraisForfait` (`id`, `libelle`, `montant`) VALUES
+('ETP', 'Forfait Etape', '110.00'),
+('KM', 'Frais Kilométrique', '0.62'),
+('NUI', 'Nuitée Hôtel', '80.00'),
+('REP', 'Repas Restaurant', '25.00');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `LigneFraisForfait`
+--
+
+CREATE TABLE `LigneFraisForfait` (
+  `idVisiteur` char(4) NOT NULL,
+  `mois` char(6) NOT NULL,
+  `idFraisForfait` char(3) NOT NULL,
+  `quantite` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `LigneFraisHorsForfait`
+--
+
+CREATE TABLE `LigneFraisHorsForfait` (
+  `id` int(11) NOT NULL,
+  `idVisiteur` char(4) NOT NULL,
+  `mois` char(6) NOT NULL,
+  `libelle` varchar(100) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `montant` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Visiteur`
+--
+
+CREATE TABLE `Visiteur` (
+  `id` char(4) NOT NULL,
+  `nom` char(30) DEFAULT NULL,
+  `prenom` char(30) DEFAULT NULL,
+  `login` char(20) DEFAULT NULL,
+  `mdp` char(20) DEFAULT NULL,
+  `adresse` char(30) DEFAULT NULL,
+  `cp` char(5) DEFAULT NULL,
+  `ville` char(30) DEFAULT NULL,
+  `dateEmbauche` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Contenu de la table `Visiteur`
@@ -55,4 +168,95 @@ INSERT INTO `Visiteur` (`id`, `nom`, `prenom`, `login`, `mdp`, `adresse`, `cp`, 
 ('f21', 'Finck', 'Jacques', 'jfinck', 'mpb3t', '10 avenue du Prado', '13002', 'Marseille', '2001-11-10'),
 ('f39', 'Frémont', 'Fernande', 'ffremont', 'xs5tq', '4 route de la mer', '13012', 'Allauh', '1998-10-01'),
 ('f4', 'Gest', 'Alain', 'agest', 'dywvt', '30 avenue de la mer', '13025', 'Berre', '1985-11-01');
--- --------------------------------------------------------
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `Absence`
+--
+ALTER TABLE `Absence`
+  ADD KEY `FK absence` (`refVisiteur`);
+
+--
+-- Index pour la table `Etat`
+--
+ALTER TABLE `Etat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `FicheFrais`
+--
+ALTER TABLE `FicheFrais`
+  ADD PRIMARY KEY (`idVisiteur`,`mois`),
+  ADD KEY `idEtat` (`idEtat`);
+
+--
+-- Index pour la table `FraisForfait`
+--
+ALTER TABLE `FraisForfait`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `LigneFraisForfait`
+--
+ALTER TABLE `LigneFraisForfait`
+  ADD PRIMARY KEY (`idVisiteur`,`mois`,`idFraisForfait`),
+  ADD KEY `idFraisForfait` (`idFraisForfait`);
+
+--
+-- Index pour la table `LigneFraisHorsForfait`
+--
+ALTER TABLE `LigneFraisHorsForfait`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idVisiteur` (`idVisiteur`,`mois`);
+
+--
+-- Index pour la table `Visiteur`
+--
+ALTER TABLE `Visiteur`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `LigneFraisHorsForfait`
+--
+ALTER TABLE `LigneFraisHorsForfait`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `Absence`
+--
+ALTER TABLE `Absence`
+  ADD CONSTRAINT `FK absence` FOREIGN KEY (`refVisiteur`) REFERENCES `Visiteur` (`id`);
+
+--
+-- Contraintes pour la table `FicheFrais`
+--
+ALTER TABLE `FicheFrais`
+  ADD CONSTRAINT `FicheFrais_ibfk_1` FOREIGN KEY (`idEtat`) REFERENCES `Etat` (`id`),
+  ADD CONSTRAINT `FicheFrais_ibfk_2` FOREIGN KEY (`idVisiteur`) REFERENCES `Visiteur` (`id`);
+
+--
+-- Contraintes pour la table `LigneFraisForfait`
+--
+ALTER TABLE `LigneFraisForfait`
+  ADD CONSTRAINT `LigneFraisForfait_ibfk_1` FOREIGN KEY (`idVisiteur`,`mois`) REFERENCES `FicheFrais` (`idVisiteur`, `mois`),
+  ADD CONSTRAINT `LigneFraisForfait_ibfk_2` FOREIGN KEY (`idFraisForfait`) REFERENCES `FraisForfait` (`id`);
+
+--
+-- Contraintes pour la table `LigneFraisHorsForfait`
+--
+ALTER TABLE `LigneFraisHorsForfait`
+  ADD CONSTRAINT `LigneFraisHorsForfait_ibfk_1` FOREIGN KEY (`idVisiteur`,`mois`) REFERENCES `FicheFrais` (`idVisiteur`, `mois`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
