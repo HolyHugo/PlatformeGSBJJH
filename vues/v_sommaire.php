6079948f8b3307ba64c763d0944c37f49d2953bd
holyhugo <hugobocktaels@gmail.com>
2017-10-05 09:53:29 +0200

    CA MARCHE : Ajout absence fonctionnel TP FINI

--- vues/v_sommaire.php
+++ vues/v_sommaire.php
@@ -20,7 +20,7 @@
            </li>
            
            <li class="smenu">
-              <a href="index.php?uc=gererAbsence&action=landing" title="Consultation de mes fiches de mes absences">Saisie absences</a>
+              <a href="index.php?uc=gererAbsence&action=creeNouvelleAbsence" title="Consultation de mes fiches de mes absences">Saisie absences</a>
            </li>
 
            <li class="smenu">
